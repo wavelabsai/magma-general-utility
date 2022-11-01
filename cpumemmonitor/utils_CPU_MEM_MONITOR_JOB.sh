564d233c54771bbf9b6794e91855d94732fd192b
Yogesh Pandey <yogesh@wavelabs.ai>
2022-11-01 07:18:39 +0000

    fix(cpumem): Fixing the output of script
    1. Tuned the output of CPU/MEM scripts
    
    Signed-off-by: Yogesh Pandey <yogesh@wavelabs.ai>

--- cpumemmonitor/utils_CPU_MEM_MONITOR_JOB.sh
+++ cpumemmonitor/utils_CPU_MEM_MONITOR_JOB.sh
@@ -2,49 +2,54 @@
 CPU_MEM_UTL_LOG_FILE=/var/core/cpu-mem-log.txt
 if [ ! -f "$CPU_MEM_UTL_LOG_FILE" ]
 then
-    sudo touch $CPU_MEM_UTL_LOG_FILE 
+    sudo touch $CPU_MEM_UTL_LOG_FILE
 fi
 
-PID_MAGMAD=$(ps -eaf | grep python3 | grep magmad | awk '{print $2}')
-PID_SUBSCRIBERDB=$(ps -eaf | grep python3 | grep subscriberdb | awk '{print $2}')
-PID_DIRECTORYD=$(ps -eaf | grep python3 | grep directoryd | awk '{print $2}')
-PID_ENODEBD=$(ps -eaf | grep python3 | grep enodebd | awk '{print $2}')
-PID_POLICYDB=$(ps -eaf | grep python3 | grep policydb | awk '{print $2}')
-PID_STATE=$(ps -eaf | grep python3 | grep state | awk '{print $2}')
-PID_CTRACED=$(ps -eaf | grep python3 | grep ctraced | awk '{print $2}')
-PID_SMSD=$(ps -eaf | grep python3 | grep smsd | awk '{print $2}')
-PID_EVENTD=$(ps -eaf | grep python3 | grep eventd | awk '{print $2}')
-PID_MOBILITYD=$(ps -eaf | grep python3 | grep mobilityd | awk '{print $2}')
-PID_PIPELINED=$(ps -eaf | grep python3 | grep pipelined | awk '{print $2}')
+print_python_cpu_mem_usage() {
+  echo "PRINTING FOR $1"
+  process_id=$1
 
-PID_MME=$(pidof mme)
-PID_SESSIOND=$(pidof sessiond)
+  PID_NEW_PROCESS=$(ps -eaf | grep python3 | grep $1 | awk 'NR==1{print $2}')
+  if [[ -z PID_NEW_PROCESS ]]; then   echo "$1 DOESNOT EXISTS"; fi
 
+  CPU_MEM_DUMP=$(ps -p $PID_NEW_PROCESS -o %cpu,%mem | awk NR==2)
+  echo $CPU_MEM_DUMP : $1" ("$PID_NEW_PROCESS")" >> $CPU_MEM_UTL_LOG_FILE
+}
+
+print_c_cpu_mem_usage() {
+
+  PID_MME=$(pidof mme)
+  PID_SESSIOND=$(pidof sessiond)
+
+  if [[ -z PID_MME ]]; then   echo "MME DOESNOT EXISTS"; fi
+  if [[ -z PID_SESSIOND ]]; then   echo "SESSIOND DOESNOT EXISTS"; fi
+
+  CPU_MEM_DUMP_MME=$(ps -p $PID_MME -o %cpu,%mem | awk NR==2)
+  echo $CPU_MEM_DUMP_MME : mme" ("$PID_MME")" >> $CPU_MEM_UTL_LOG_FILE
+
+  CPU_MEM_DUMP_SESSIOND=$(ps -p $PID_SESSIOND -o %cpu,%mem | awk NR==2)
+  echo $CPU_MEM_DUMP_SESSIOND : sessiond" ("$PID_SESSIOND")" >> $CPU_MEM_UTL_LOG_FILE
+}
+
+echo " " >> $CPU_MEM_UTL_LOG_FILE
 date >> $CPU_MEM_UTL_LOG_FILE
-echo "_________________________________________________" >> $CPU_MEM_UTL_LOG_FILE
-echo "MAGMA :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_MAGMAD -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE 
-echo "SUBSCRIBERDB :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_SUBSCRIBERDB -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "DIRECTORYD :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_DIRECTORYD  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "ENODEBD :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_ENODEBD  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "POLICYDB :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_POLICYDB  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "STATE :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_STATE  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "CTRACED :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_CTRACED  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "SMSD :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_SMSD  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "EVEND : " >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_EVENTD  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "MOBILITYD :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_MOBILITYD  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "PIPELINED :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_PIPELINED  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "MME :" >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_MME  -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
-echo "SESSIOND : " >> $CPU_MEM_UTL_LOG_FILE
-  ps -p $PID_SESSIOND -o %cpu,%mem >> $CPU_MEM_UTL_LOG_FILE
+echo "CPU   MEM    PROCESS" >> $CPU_MEM_UTL_LOG_FILE
+echo "=========================" >> $CPU_MEM_UTL_LOG_FILE
+
+#Printing for Python Files
+print_python_cpu_mem_usage magmad
+print_python_cpu_mem_usage subscriberdb
+print_python_cpu_mem_usage directoryd
+print_python_cpu_mem_usage enodebd
+print_python_cpu_mem_usage policydb
+print_python_cpu_mem_usage state
+print_python_cpu_mem_usage ctraced
+print_python_cpu_mem_usage smsd
+print_python_cpu_mem_usage eventd
+print_python_cpu_mem_usage mobilityd
+print_python_cpu_mem_usage pipelined
+
+#Printing for C files
+print_c_cpu_mem_usage
+
+echo " ---- Finished Logging ----" >> $CPU_MEM_UTL_LOG_FILE
