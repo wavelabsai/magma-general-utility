c8e48dacd7158679153cd3740e9ea9096237d18b
mehul-jindal-wavelabs <mehul.jindal@wavelabs.ai>
2023-06-16 18:31:31 +0530

    feat(flask-handler): add support for deletion
    Signed-off-by: mehul-jindal-wavelabs <mehul.jindal@wavelabs.ai>

--- pmn-systems-confs/flask-api-handler/flask_handler.py
+++ pmn-systems-confs/flask-api-handler/flask_handler.py
@@ -1,93 +1,158 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisioned-data/am-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provis"
+           "ioned-data/am-data", methods=["PUT", "DELETE"])
 def am_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "am-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "am-data": request.json
+        }
+    else:
+        return {"Deleted am-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisioned-data/smf-selection-subscription-data",
-           methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provis"
+           "ioned-data/smf-selection-subscription-data",
+           methods=["PUT", "DELETE"])
 def smf_selection_subscription_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "smf-selection-subscription-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "smf-selection-subscription-data": request.json
+        }
+    else:
+        return {"Deleted smf-selection-subscription-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/sm-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/sm-data",
+           methods=["PUT", "DELETE"])
 def policy_sm_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "sm-data-policy": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "sm-data-policy": request.json
+        }
+    else:
+        return {"Deleted sm-data-policy for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/authentication-subscription", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/authen"
+           "tication-subscription", methods=["PUT", "DELETE"])
 def authentication_subscription(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "auth-subs-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "auth-subs-data": request.json
+        }
+    else:
+        return {"Deleted auth-subs-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/operator-specific-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/operator-speci"
+           "fic-data", methods=["PUT", "DELETE"])
 def operator_specific_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "osd": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "osd": request.json
+        }
+    else:
+        return {"Deleted osd for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisioned-data/sm-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisio"
+           "ned-data/sm-data", methods=["PUT", "DELETE"])
 def sm_data(supi, imsi):
-    pass
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "sm-data": request.json
+        }
+    else:
+        return {"Deleted sm-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/am-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/am-data",
+           methods=["PUT", "DELETE"])
 def policy_am_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "am-policy-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "am-policy-data": request.json
+        }
+    else:
+        return {"Deleted am-policy-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/ue-policy-set", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/policy-data/ue-policy-set",
+           methods=["PUT", "DELETE"])
 def ue_policy_set(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "ue-policy-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "ue-policy-data": request.json
+        }
+    else:
+        return {"Deleted ue-policy-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisioned-data/sms-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisi"
+           "oned-data/sms-data", methods=["PUT", "DELETE"])
 def sms_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "sms-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "sms-data": request.json
+        }
+    else:
+        return {"Deleted sms-data for": supi}
 
 
-@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisioned-data/sms-mng-data", methods=["PUT"])
+@app.route("/nudr-sp/v1/subs-<supi>/5gs/imsi-<imsi>/subscription-data/provisi"
+           "oned-data/sms-mng-data", methods=["PUT", "DELETE"])
 def sms_mng_data(supi, imsi):
-    return {
-        "supi": supi,
-        "imsi": imsi,
-        "sms-mng-data": request.json
-    }
+    if request.method == 'PUT':
+        json_object = json.dumps(request.json, indent=4)
+        print(json_object)
+        return {
+            "supi": supi,
+            "imsi": imsi,
+            "sms-mng-data": request.json
+        }
+    else:
+        return {"Deleted sms-mng-data for": supi}
 
 
 if __name__ == '__main__':
