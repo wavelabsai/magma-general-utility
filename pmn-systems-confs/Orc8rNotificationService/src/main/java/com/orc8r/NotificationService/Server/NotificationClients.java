5bfcbd3134b0aab0dd8bf196a61a39446ce2adb5
mehul-jindal <mehul.jindal@veltris.com>
2024-01-05 17:46:44 +0530

    pmn-system-confs: NotificationServer SubsDBCache Notifier
    Signed-off-by: mehul-jindal <mehul.jindal@veltris.com>

--- pmn-systems-confs/Orc8rNotificationService/src/main/java/com/orc8r/NotificationService/Server/NotificationClients.java
+++ pmn-systems-confs/Orc8rNotificationService/src/main/java/com/orc8r/NotificationService/Server/NotificationClients.java
@@ -10,6 +10,8 @@ public class NotificationClients {
     private Map<String, Map<String, ClientRequestHandler>> clientMap
             = new ConcurrentHashMap<String, Map<String, ClientRequestHandler>>();
 
+    private ClientRequestHandler subscribreDigest = null;
+
     public NotificationClients() {
         Map<String, ClientRequestHandler> defaultMap = new ConcurrentHashMap<String, ClientRequestHandler>();
         clientMap.put("default",defaultMap);
@@ -25,6 +27,13 @@ public class NotificationClients {
         System.out.println(map);
     }
 
+    public void addSubscriberDigest(ClientRequestHandler handler) {
+        if (this.subscribreDigest != null) {
+            this.subscribreDigest.closeConnection();
+        }
+        this.subscribreDigest = handler;
+    }
+
     public void removeClient(ClientRequestHandler requestHandler) {
         Map<String, ClientRequestHandler> map = clientMap.get(requestHandler.getNetwork());
         ClientRequestHandler handler = map.get(requestHandler.getGateway());
@@ -33,7 +42,28 @@ public class NotificationClients {
 
     }
 
+    public void sendDigestRequest() {
+        if (subscribreDigest != null) {
+            System.out.println("Sending Subscriber change request to Digester");
+            if (!subscribreDigest.isClosed()) {
+                subscribreDigest.sendNotification();
+            } else {
+                System.out.println("Seems like digester is closed... Notification failed");
+            }
+        } else {
+            System.out.println("No digester registered... Notification failed");
+        }
+    }
+
     public void postMessage(ClientRequestHandler requestHandler) {
+        System.out.println("Received a Subscriber change request");
+        this.sendDigestRequest();
+        try {
+            System.out.println("Waiting for 3 seconds before forwarding the request..");
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
         System.out.println("Forwarding the post to the registered Users");
         Map<String, ClientRequestHandler> map = clientMap.get(requestHandler.getNetwork());
         for (Map.Entry<String, ClientRequestHandler> entry : map.entrySet()) {
